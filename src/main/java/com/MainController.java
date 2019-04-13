f3727fab31bd9ba1d3d3834286b36e7b2b275e93
ChenZiang16 <withchenziang@163.com>
2019-04-13 18:18:03 +0800

    ok

---
+++ src/main/java/com/MainController.java
@@ -0,0 +1,13 @@
+package com;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	@RequestMapping("/testJenkins")
+	@ResponseBody
+	public String message() {
+		return "request is recived !";
+	}
+}
